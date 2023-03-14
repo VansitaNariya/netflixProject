8af66595aa1eae6059971baa32c291346b9b692d
vansita nariya <vansitanariya82@gmail.com>
2023-03-14 12:15:29 +0530

    Initial Commit

---
+++ netflixProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflixProject
+//
+//  Created by R92 on 14/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
